d58684513313789323122cd60b65aab078611833
yoelsheikh <yoelsheikh@gmail.com>
2024-12-11 16:18:27 +0100

    add some comments

--- to_be_copied_to_container/slurm_command.sh
+++ to_be_copied_to_container/slurm_command.sh
@@ -2,13 +2,13 @@
 PIPE_PATH="/home/mypipe"
 OUTPUT_PIPE="/home/outputpipe"
 
-# slurm command (e.g. squeue, sbatch, etc.)
+# slurm command (e.g. squeue, sbatch, etc.) which is an argumet for this script
 SLURM_COMMAND=$1
 
 # pass it to the pipe in skynet
 echo "$SLURM_COMMAND" > "$PIPE_PATH"
 
-# ait for and print the output
+# wai for and print the output, i.e. so that the container user can see if there are errors
 if [[ -p $OUTPUT_PIPE ]]; then
   while read -r line; do
     echo "$line"
@@ -16,3 +16,4 @@ if [[ -p $OUTPUT_PIPE ]]; then
 else
   echo "Output pipe $OUTPUT_PIPE does not exist."
 fi
+
