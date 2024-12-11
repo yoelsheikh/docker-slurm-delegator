0afb81b3467e6be991454bf2496179b1000a08ae
yoelsheikh <yoelsheikh@gmail.com>
2024-12-11 16:16:48 +0100

    split pipe args and carry those in an env and script env vars

--- slurm_listener.sh
+++ slurm_listener.sh
@@ -2,19 +2,19 @@
 # named pipes created via mkfifo and are attached to the container in the config
 PIPE_PATH="/home/skynet/pipe/mypipe"
 OUTPUT_PIPE="/home/skynet/pipe/outputpipe"
-OUTPUT_PIPE_CONTAINER="/home/outputpipe"
+
 
 while true; do
-  if read command < "$PIPE_PATH"; then
-    if [[ $command == "squeue" ]]; then
-      # run squeue and send output back
-      squeue > "$OUTPUT_PIPE"
-    else
-      # default to executing a job - will later be executed inside of an sbatch
-      docker exec -w /home/jovyan/work jupyter-yousif \
-        bash -c "source /opt/conda/bin/activate bas && python $command" \
-        > "$OUTPUT_PIPE" 2>&1
-    fi
+if read command < "$PIPE_PATH"; then
+  IFS=" " read -r CMD ARGS <<< "$command"
+  if [[ $CMD == "squeue" ]]; then
+    squeue > "$OUTPUT_PIPE"
+  else
+    ENV_NAME=$CMD
+    PY_SCRIPT_PATH=$ARGS
+    docker exec -w /home/jovyan/work jupyter-yousif \
+      bash -c "source /opt/conda/bin/activate $ENV_NAME && python $PY_SCRIPT_PATH" \
+      > "$OUTPUT_PIPE" 2>&1
   fi
+fi
 done
-
