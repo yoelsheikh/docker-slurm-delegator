12cadc4fde1b09e4b07acc8c2a1f0662df7c8966
yoelsheikh <yoelsheikh@gmail.com>
2024-12-11 17:02:19 +0100

    edit env vars names and read container id as an env var

--- slurm_listener.sh
+++ slurm_listener.sh
@@ -6,13 +6,15 @@ OUTPUT_PIPE="/home/skynet/pipe/outputpipe"
 
 while true; do
 if read command < "$PIPE_PATH"; then
-  IFS=" " read -r CMD ARGS <<< "$command"
+  IFS=" " read -r ALPH BETA GAMM <<< "$command"
+  # debug
+  echo "ALPH: $ALPH, BETA: $BETA, GAMM: $GAMM"
   if [[ $CMD == "squeue" ]]; then
     squeue > "$OUTPUT_PIPE"
   else
-    ENV_NAME=$CMD
-    PY_SCRIPT_PATH=$ARGS
-    docker exec -w /home/jovyan/work jupyter-yousif \
+    ENV_NAME=$BETA
+    PY_SCRIPT_PATH=$GAMM
+    docker exec -w /home/jovyan/work $ALPH \
       bash -c "source /opt/conda/bin/activate $ENV_NAME && python $PY_SCRIPT_PATH" \
       > "$OUTPUT_PIPE" 2>&1
   fi
